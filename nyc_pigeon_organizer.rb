3f55ed66fe033cac1e87fad263fa242bee198a07
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-14 23:36:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |cgl, cgl_value|
+    cgl_value.each do |categories, names|
+      names.each do |name|
+        hash.store(name, {:color => [], :gender => [], :lives => []})
+      end
+    end
+  end
+  data[:color].each do |category, names|
+    names.each do |name|
+      hash[name][:color] << category.to_s
+    end
+  end
+  data[:gender].each do |category, names|
+    names.each do |name|
+      hash[name][:gender] << category.to_s
+    end
+  end
+  data[:lives].each do |category, names|
+    names.each do |name|
+      hash[name][:lives] << category.to_s
+    end
+  end
+  hash
 end
